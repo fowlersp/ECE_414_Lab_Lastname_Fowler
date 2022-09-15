e4913011eb50ef1ce0fd9c91b4452b7842822674
Jason Sonkin <sonkinj@ad.lafayette.edu>
2022-09-15 10:42:37 -0400

    first lab3 commit

---
+++ Lab3/src/porta_in.h
@@ -0,0 +1,17 @@
+/* 
+ * File:   porta_in.h
+ * Author: spfow
+ *
+ * Created on September 7, 2022, 10:38 PM
+ */
+
+#ifndef PORTA_IN_H
+#define	PORTA_IN_H
+
+#include <inttypes.h>
+
+extern void porta_in_init();
+extern uint8_t porta_in_read();
+
+#endif
+
